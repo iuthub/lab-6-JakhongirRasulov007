32324c4a748ab7287cffc205a83979fcf5a2c734
jakhongirrasulov007 <jakhongirrasulov001@gmail.com>
2021-04-04 22:21:32 +0500

    "email" is complete

--- auth/task2.php
+++ auth/task2.php
@@ -12,12 +12,20 @@
     // Name This field is required. It has to contain at least 2 chars. It should not contain any number.
     $name ="";
     $isNameValid = false;
+    $isEmailValid = false;
     if ($_SERVER["REQUEST_METHOD"] = "POST"){
         //check name
         $namePattern = "/(.*[a-z]){2}/i";
         $name = $_REQUEST["name"];
         $isNameValid = preg_match($namePattern,$name);
+
+        // checking email
+        $email = $_REQUEST["email"];
+        $emailPattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+        $isEmailValid = preg_match("/$emailPattern/", $email);
     }
+
+
 
 
 
@@ -30,6 +38,15 @@
             <label for="name" class="col-sm-2 col-form-label">name</label>
             <div class="col-sm-10">
                 <input type="text" class="form-control <?=$isNameValid ? "": "is-invalid"?>" id="name" name="name" placeholder="enter name">
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row mb-3">
+            <label for="email" class="col-sm-2 col-form-label">email</label>
+            <div class="col-sm-10">
+                <input type="email" class="form-control <?=$isEmailValid ? "": "is-invalid"?>" id="name" name="email" placeholder="enter email">
             </div>
         </div>
     </div>
